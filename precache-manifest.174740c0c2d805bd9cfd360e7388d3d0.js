61c3749d146092c26b9a5ab177a1e8f51dc4477c
ruben <ruben@globatalent.com>
2019-01-16 17:06:47 +0100

    Updates

---
+++ precache-manifest.174740c0c2d805bd9cfd360e7388d3d0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "27e34b046cad074bcf164aff148cd74e",
+    "url": "/static/media/info_3.27e34b04.png"
+  },
+  {
+    "revision": "fa632ae4a6a4f2d68dbf414009414359",
+    "url": "/static/media/info_2.fa632ae4.png"
+  },
+  {
+    "revision": "977fb49d8fac82d063d5e0e48f8e2bed",
+    "url": "/static/media/info_1.977fb49d.png"
+  },
+  {
+    "revision": "d8e7a98a719009395bd9b2eacf177ae2",
+    "url": "/static/media/hero.d8e7a98a.png"
+  },
+  {
+    "revision": "568b9d663d76e90506b20cc1ddf74988",
+    "url": "/static/media/cta.568b9d66.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0dbf4979ba7438bf43b7",
+    "url": "/static/js/main.0dbf4979.chunk.js"
+  },
+  {
+    "revision": "531039200bdb72d760ad",
+    "url": "/static/js/1.53103920.chunk.js"
+  },
+  {
+    "revision": "0dbf4979ba7438bf43b7",
+    "url": "/static/css/main.d8716ba2.chunk.css"
+  },
+  {
+    "revision": "fd5326e2bb17321e0d859b043abe6a7d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/b2bpromo/precache-manifest.8b84aa67f8ec51ba9c92bbe5ca58e68d.js"
+  "/precache-manifest.174740c0c2d805bd9cfd360e7388d3d0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/b2bpromo/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
