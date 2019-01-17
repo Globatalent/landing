8c40a3baa2bebe43ea6c1c86656735289fd569b2
ruben <ruben@globatalent.com>
2019-01-17 06:54:15 +0100

    Updates

---
+++ precache-manifest.9b328000a239890c08300a0302e14382.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "568b9d663d76e90506b20cc1ddf74988",
+    "url": "./b2bpromo/static/media/cta.568b9d66.png"
+  },
+  {
+    "revision": "583599ba77e908b532df",
+    "url": "./b2bpromo/static/css/main.c14654a3.chunk.css"
+  },
+  {
+    "revision": "d47bd2a36bc69604b8a5",
+    "url": "./b2bpromo/static/js/1.d47bd2a3.chunk.js"
+  },
+  {
+    "revision": "6a48f09480ca55bf4f65",
+    "url": "./b2bpromo/static/js/runtime~main.6a48f094.js"
+  },
+  {
+    "revision": "d8e7a98a719009395bd9b2eacf177ae2",
+    "url": "./b2bpromo/static/media/hero.d8e7a98a.png"
+  },
+  {
+    "revision": "977fb49d8fac82d063d5e0e48f8e2bed",
+    "url": "./b2bpromo/static/media/info_1.977fb49d.png"
+  },
+  {
+    "revision": "fa632ae4a6a4f2d68dbf414009414359",
+    "url": "./b2bpromo/static/media/info_2.fa632ae4.png"
+  },
+  {
+    "revision": "27e34b046cad074bcf164aff148cd74e",
+    "url": "./b2bpromo/static/media/info_3.27e34b04.png"
+  },
+  {
+    "revision": "583599ba77e908b532df",
+    "url": "./b2bpromo/static/js/main.583599ba.chunk.js"
+  },
+  {
+    "revision": "c15ae8399181b6d099ea99f555392ebe",
+    "url": "./b2bpromo/static/media/card_1.c15ae839.png"
+  },
+  {
+    "revision": "11884515a0a82d212dd42c79bf49e2ee",
+    "url": "./b2bpromo/static/media/card_2.11884515.png"
+  },
+  {
+    "revision": "29281f1489e811d462158ff894a3de70",
+    "url": "./b2bpromo/static/media/card_3.29281f14.png"
+  },
+  {
+    "revision": "3f03894d5b8e5fc552b94e4ead7131de",
+    "url": "./b2bpromo/static/media/supp_1.3f03894d.png"
+  },
+  {
+    "revision": "34cd617246d1f347244100dc7dd3e366",
+    "url": "./b2bpromo/static/media/supp_2.34cd6172.png"
+  },
+  {
+    "revision": "a0020f0f61fed1c072e22aec0fa514eb",
+    "url": "./b2bpromo/static/media/supp_3.a0020f0f.png"
+  },
+  {
+    "revision": "0d290e59d300af4352869b737ec1943b",
+    "url": "./b2bpromo/static/media/supp_4.0d290e59.png"
+  },
+  {
+    "revision": "caaf93721694650ce8cbae3fa3785684",
+    "url": "./b2bpromo/index.html"
+  }
+];
