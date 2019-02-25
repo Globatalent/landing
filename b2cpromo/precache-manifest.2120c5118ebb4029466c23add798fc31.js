a71458d34c4c198c18673c86771855e199825039
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-02-25 07:14:08 +0100

    updated b2c

---
+++ b2cpromo/precache-manifest.2120c5118ebb4029466c23add798fc31.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a3a00ebcb6d9f827b70536212421b9b",
+    "url": "/b2cpromo/static/media/campaign_3.7a3a00eb.png"
+  },
+  {
+    "revision": "138744e9e520188da53b",
+    "url": "/b2cpromo/static/css/main.2d9afb12.chunk.css"
+  },
+  {
+    "revision": "2ed5eba86d951e217db6",
+    "url": "/b2cpromo/static/js/1.2ed5eba8.chunk.js"
+  },
+  {
+    "revision": "1de0aed82685a4681c95",
+    "url": "/b2cpromo/static/js/runtime~main.1de0aed8.js"
+  },
+  {
+    "revision": "990f8f3a61b2e46af89c4bbc09e1ce09",
+    "url": "/b2cpromo/static/media/hero.990f8f3a.jpg"
+  },
+  {
+    "revision": "8735af26b74e1be109bfbf767cc3baa2",
+    "url": "/b2cpromo/static/media/campaign_1.8735af26.png"
+  },
+  {
+    "revision": "1877842a64a2e831484f999f0955fb36",
+    "url": "/b2cpromo/static/media/campaign_2.1877842a.png"
+  },
+  {
+    "revision": "138744e9e520188da53b",
+    "url": "/b2cpromo/static/js/main.138744e9.chunk.js"
+  },
+  {
+    "revision": "c27b279c3fb2538ca8bdda0d7f89bfdf",
+    "url": "/b2cpromo/static/media/supp_1.c27b279c.png"
+  },
+  {
+    "revision": "327654da90de0f7dd27ce686e0c77ef0",
+    "url": "/b2cpromo/static/media/supp_2.327654da.png"
+  },
+  {
+    "revision": "0d913f8a270b4cf90e42e955365860ff",
+    "url": "/b2cpromo/static/media/supp_3.0d913f8a.png"
+  },
+  {
+    "revision": "3b7476c78f5087fdc318c7ccf54088a3",
+    "url": "/b2cpromo/static/media/supp_4.3b7476c7.png"
+  },
+  {
+    "revision": "785388a9e6416c286ec82ff966c696fd",
+    "url": "/b2cpromo/static/media/Aller_Rg.785388a9.ttf"
+  },
+  {
+    "revision": "64338493bc5b51b92e87024e33e29dbb",
+    "url": "/b2cpromo/index.html"
+  }
+];
--- misc/b2cpromo/service-worker.js
+++ misc/b2cpromo/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/b2cpromo/precache-manifest.2120c5118ebb4029466c23add798fc31.js"
+  "/b2cpromo/precache-manifest.8673a777cc14c483d0fc1481dd36eecf.js"
 );
 
 workbox.clientsClaim();
