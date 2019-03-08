de9a35fbfab3d848afeb9f3a48aae612568d10d3
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-03-08 08:51:15 +0100

    vega

---
+++ vega/precache-manifest.ba9ebf052b74838432437fe8101edab8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c27b279c3fb2538ca8bdda0d7f89bfdf",
+    "url": "/vega/static/media/supp_1.c27b279c.png"
+  },
+  {
+    "revision": "8eefd54355fb34b3abcb",
+    "url": "/vega/static/css/main.aa8a1f67.chunk.css"
+  },
+  {
+    "revision": "c2a99b2457fb1142fbcf",
+    "url": "/vega/static/js/1.c2a99b24.chunk.js"
+  },
+  {
+    "revision": "5950299b42a8266c5193",
+    "url": "/vega/static/js/runtime~main.5950299b.js"
+  },
+  {
+    "revision": "ad0355ce6f48bea0946c59be8f96aa5b",
+    "url": "/vega/static/media/hero.ad0355ce.png"
+  },
+  {
+    "revision": "8eefd54355fb34b3abcb",
+    "url": "/vega/static/js/main.8eefd543.chunk.js"
+  },
+  {
+    "revision": "327654da90de0f7dd27ce686e0c77ef0",
+    "url": "/vega/static/media/supp_2.327654da.png"
+  },
+  {
+    "revision": "0d913f8a270b4cf90e42e955365860ff",
+    "url": "/vega/static/media/supp_3.0d913f8a.png"
+  },
+  {
+    "revision": "3b7476c78f5087fdc318c7ccf54088a3",
+    "url": "/vega/static/media/supp_4.3b7476c7.png"
+  },
+  {
+    "revision": "785388a9e6416c286ec82ff966c696fd",
+    "url": "/vega/static/media/Aller_Rg.785388a9.ttf"
+  },
+  {
+    "revision": "8893a8376486d12d9d9a68339bceb29b",
+    "url": "/vega/index.html"
+  }
+];
