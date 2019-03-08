4f40cedc4f0f0c008c4dd260b2111ecca0964a53
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-03-08 09:43:31 +0100

    zentro

---
+++ zentro/precache-manifest.a01e59fe26f3509c3feb5e50367d7efb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c27b279c3fb2538ca8bdda0d7f89bfdf",
+    "url": "/zentro/static/media/supp_1.c27b279c.png"
+  },
+  {
+    "revision": "847c48ef4cde04771f38",
+    "url": "/zentro/static/css/main.c84bae0a.chunk.css"
+  },
+  {
+    "revision": "63f1e6fd0f3d80685275",
+    "url": "/zentro/static/js/1.63f1e6fd.chunk.js"
+  },
+  {
+    "revision": "f929bf04deacc476d3d9",
+    "url": "/zentro/static/js/runtime~main.f929bf04.js"
+  },
+  {
+    "revision": "0f24ba828f9a6d3d675c81d5ff755eb4",
+    "url": "/zentro/static/media/hero.0f24ba82.png"
+  },
+  {
+    "revision": "847c48ef4cde04771f38",
+    "url": "/zentro/static/js/main.847c48ef.chunk.js"
+  },
+  {
+    "revision": "327654da90de0f7dd27ce686e0c77ef0",
+    "url": "/zentro/static/media/supp_2.327654da.png"
+  },
+  {
+    "revision": "0d913f8a270b4cf90e42e955365860ff",
+    "url": "/zentro/static/media/supp_3.0d913f8a.png"
+  },
+  {
+    "revision": "3b7476c78f5087fdc318c7ccf54088a3",
+    "url": "/zentro/static/media/supp_4.3b7476c7.png"
+  },
+  {
+    "revision": "785388a9e6416c286ec82ff966c696fd",
+    "url": "/zentro/static/media/Aller_Rg.785388a9.ttf"
+  },
+  {
+    "revision": "840b361bae5b1fbd21432144c0a627a0",
+    "url": "/zentro/index.html"
+  }
+];
