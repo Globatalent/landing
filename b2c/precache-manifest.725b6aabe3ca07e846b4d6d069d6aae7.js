cac1b39cc649aa2a87656aa923360f7c96dee8a2
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-03-11 11:25:25 +0100

    b2c

---
+++ b2c/precache-manifest.725b6aabe3ca07e846b4d6d069d6aae7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "327654da90de0f7dd27ce686e0c77ef0",
+    "url": "/b2c/static/media/supp_2.327654da.png"
+  },
+  {
+    "revision": "a0d4c5fb4fbab3c8475c",
+    "url": "/b2c/static/css/main.2b95ac57.chunk.css"
+  },
+  {
+    "revision": "b6be3b7f5689c7c99a38",
+    "url": "/b2c/static/js/1.b6be3b7f.chunk.js"
+  },
+  {
+    "revision": "52aa4afea69d971d0c81",
+    "url": "/b2c/static/js/runtime~main.52aa4afe.js"
+  },
+  {
+    "revision": "6764a1f3a50559388ced290f39296f12",
+    "url": "/b2c/static/media/hero.6764a1f3.jpg"
+  },
+  {
+    "revision": "c27b279c3fb2538ca8bdda0d7f89bfdf",
+    "url": "/b2c/static/media/supp_1.c27b279c.png"
+  },
+  {
+    "revision": "a0d4c5fb4fbab3c8475c",
+    "url": "/b2c/static/js/main.a0d4c5fb.chunk.js"
+  },
+  {
+    "revision": "0d913f8a270b4cf90e42e955365860ff",
+    "url": "/b2c/static/media/supp_3.0d913f8a.png"
+  },
+  {
+    "revision": "3b7476c78f5087fdc318c7ccf54088a3",
+    "url": "/b2c/static/media/supp_4.3b7476c7.png"
+  },
+  {
+    "revision": "785388a9e6416c286ec82ff966c696fd",
+    "url": "/b2c/static/media/Aller_Rg.785388a9.ttf"
+  },
+  {
+    "revision": "f8ad016722fe04c29eb0ad3a12fcdfc7",
+    "url": "/b2c/static/media/Aller_It.f8ad0167.ttf"
+  },
+  {
+    "revision": "61e583d22611e370cf06c5c2baf8947c",
+    "url": "/b2c/index.html"
+  }
+];
