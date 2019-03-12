7fd55120bdeb79149b0e5c3ca867f87fe129c94d
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-03-12 08:41:29 +0100

    up

--- b2c/precache-manifest.a2ad8c7f3949451632d4f57bf5d63c1d.js
+++ b2c/precache-manifest.a2ad8c7f3949451632d4f57bf5d63c1d.js
@@ -1,15 +1,15 @@
 self.__precacheManifest = [
   {
-    "revision": "327654da90de0f7dd27ce686e0c77ef0",
-    "url": "/b2c/static/media/supp_2.327654da.png"
+    "revision": "36c7230e976b140d7a3be96bd2c3a67e",
+    "url": "/b2c/static/media/zentro.36c7230e.jpg"
   },
   {
-    "revision": "9e93caefeeea994414f0",
-    "url": "/b2c/static/css/main.2b95ac57.chunk.css"
+    "revision": "77d7197a6921285fc270",
+    "url": "/b2c/static/css/main.8b592a93.chunk.css"
   },
   {
-    "revision": "b6be3b7f5689c7c99a38",
-    "url": "/b2c/static/js/1.b6be3b7f.chunk.js"
+    "revision": "b5f1e6c5e8889d0a623d",
+    "url": "/b2c/static/js/1.b5f1e6c5.chunk.js"
   },
   {
     "revision": "52aa4afea69d971d0c81",
@@ -20,12 +20,24 @@ self.__precacheManifest = [
     "url": "/b2c/static/media/hero.6764a1f3.jpg"
   },
   {
+    "revision": "e939c85f4c3e5acce84abe2bf2e94206",
+    "url": "/b2c/static/media/epfc.e939c85f.jpg"
+  },
+  {
+    "revision": "1e58b887bddaeee5511b7e30222c0e0c",
+    "url": "/b2c/static/media/vega.1e58b887.JPG"
+  },
+  {
+    "revision": "77d7197a6921285fc270",
+    "url": "/b2c/static/js/main.77d7197a.chunk.js"
+  },
+  {
     "revision": "c27b279c3fb2538ca8bdda0d7f89bfdf",
     "url": "/b2c/static/media/supp_1.c27b279c.png"
   },
   {
-    "revision": "9e93caefeeea994414f0",
-    "url": "/b2c/static/js/main.9e93caef.chunk.js"
+    "revision": "327654da90de0f7dd27ce686e0c77ef0",
+    "url": "/b2c/static/media/supp_2.327654da.png"
   },
   {
     "revision": "0d913f8a270b4cf90e42e955365860ff",
@@ -44,7 +56,7 @@ self.__precacheManifest = [
     "url": "/b2c/static/media/Aller_Rg.785388a9.ttf"
   },
   {
-    "revision": "241cdda2900f9a938a5519c3c6276b6f",
+    "revision": "0a84ca288ab7a14da5d2925238e34b98",
     "url": "/b2c/index.html"
   }
 ];
--- b2c/service-worker.js
+++ b2c/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/b2c/precache-manifest.20e2b67aa47e334c4c4086558fdcbbad.js"
+  "/b2c/precache-manifest.a2ad8c7f3949451632d4f57bf5d63c1d.js"
 );
 
 workbox.clientsClaim();
