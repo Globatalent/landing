46b06022170a93c5cf307b19d6fd63f770c7e365
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-03-18 12:32:31 +0100

    b2cinvest

---
+++ b2cinvest/precache-manifest.a55f3db0dee3ec875881f96abfe0b56c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "c27b279c3fb2538ca8bdda0d7f89bfdf",
+    "url": "/b2cinvest/static/media/supp_1.c27b279c.png"
+  },
+  {
+    "revision": "0ceaec8986b49e76d472",
+    "url": "/b2cinvest/static/css/main.26382ee9.chunk.css"
+  },
+  {
+    "revision": "76b28c7c85ce07efa351",
+    "url": "/b2cinvest/static/js/1.76b28c7c.chunk.js"
+  },
+  {
+    "revision": "e44d4c3a5c0ab5c30d01",
+    "url": "/b2cinvest/static/js/runtime~main.e44d4c3a.js"
+  },
+  {
+    "revision": "6764a1f3a50559388ced290f39296f12",
+    "url": "/b2cinvest/static/media/hero.6764a1f3.jpg"
+  },
+  {
+    "revision": "1e58b887bddaeee5511b7e30222c0e0c",
+    "url": "/b2cinvest/static/media/vega.1e58b887.JPG"
+  },
+  {
+    "revision": "36c7230e976b140d7a3be96bd2c3a67e",
+    "url": "/b2cinvest/static/media/zentro.36c7230e.jpg"
+  },
+  {
+    "revision": "58b30c2c2d6b6724b3a75482212f3ca1",
+    "url": "/b2cinvest/static/media/fighter.58b30c2c.jpg"
+  },
+  {
+    "revision": "0ceaec8986b49e76d472",
+    "url": "/b2cinvest/static/js/main.0ceaec89.chunk.js"
+  },
+  {
+    "revision": "327654da90de0f7dd27ce686e0c77ef0",
+    "url": "/b2cinvest/static/media/supp_2.327654da.png"
+  },
+  {
+    "revision": "0d913f8a270b4cf90e42e955365860ff",
+    "url": "/b2cinvest/static/media/supp_3.0d913f8a.png"
+  },
+  {
+    "revision": "3b7476c78f5087fdc318c7ccf54088a3",
+    "url": "/b2cinvest/static/media/supp_4.3b7476c7.png"
+  },
+  {
+    "revision": "e939c85f4c3e5acce84abe2bf2e94206",
+    "url": "/b2cinvest/static/media/epfc.e939c85f.jpg"
+  },
+  {
+    "revision": "f8ad016722fe04c29eb0ad3a12fcdfc7",
+    "url": "/b2cinvest/static/media/Aller_It.f8ad0167.ttf"
+  },
+  {
+    "revision": "785388a9e6416c286ec82ff966c696fd",
+    "url": "/b2cinvest/static/media/Aller_Rg.785388a9.ttf"
+  },
+  {
+    "revision": "ccf30664f33d5db71e149bb1be40ceaf",
+    "url": "/b2cinvest/index.html"
+  }
+];
