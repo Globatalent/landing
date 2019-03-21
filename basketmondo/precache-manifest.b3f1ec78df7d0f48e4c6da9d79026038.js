1d144537fb8a668a7de4d1ec07d55ad16c8db492
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-03-21 11:09:47 +0100

    up

---
+++ basketmondo/precache-manifest.b3f1ec78df7d0f48e4c6da9d79026038.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "58b30c2c2d6b6724b3a75482212f3ca1",
+    "url": "/basketmondo/static/media/fighter.58b30c2c.jpg"
+  },
+  {
+    "revision": "dc420e3523640d07f598",
+    "url": "/basketmondo/static/css/main.6726fefc.chunk.css"
+  },
+  {
+    "revision": "b97ab106bd4732b4fcaf",
+    "url": "/basketmondo/static/js/1.b97ab106.chunk.js"
+  },
+  {
+    "revision": "3db0a15f0906ce00b8f1",
+    "url": "/basketmondo/static/js/runtime~main.3db0a15f.js"
+  },
+  {
+    "revision": "7f3914060d8c5e7a2cdcc1e069c07e64",
+    "url": "/basketmondo/static/media/hero.7f391406.jpg"
+  },
+  {
+    "revision": "07cc59fb9f0ee9542784e81abe7d361c",
+    "url": "/basketmondo/static/media/basketmondo.07cc59fb.PNG"
+  },
+  {
+    "revision": "1e58b887bddaeee5511b7e30222c0e0c",
+    "url": "/basketmondo/static/media/vega.1e58b887.JPG"
+  },
+  {
+    "revision": "36c7230e976b140d7a3be96bd2c3a67e",
+    "url": "/basketmondo/static/media/zentro.36c7230e.jpg"
+  },
+  {
+    "revision": "dc420e3523640d07f598",
+    "url": "/basketmondo/static/js/main.dc420e35.chunk.js"
+  },
+  {
+    "revision": "c27b279c3fb2538ca8bdda0d7f89bfdf",
+    "url": "/basketmondo/static/media/supp_1.c27b279c.png"
+  },
+  {
+    "revision": "327654da90de0f7dd27ce686e0c77ef0",
+    "url": "/basketmondo/static/media/supp_2.327654da.png"
+  },
+  {
+    "revision": "0d913f8a270b4cf90e42e955365860ff",
+    "url": "/basketmondo/static/media/supp_3.0d913f8a.png"
+  },
+  {
+    "revision": "3b7476c78f5087fdc318c7ccf54088a3",
+    "url": "/basketmondo/static/media/supp_4.3b7476c7.png"
+  },
+  {
+    "revision": "e939c85f4c3e5acce84abe2bf2e94206",
+    "url": "/basketmondo/static/media/epfc.e939c85f.jpg"
+  },
+  {
+    "revision": "f8ad016722fe04c29eb0ad3a12fcdfc7",
+    "url": "/basketmondo/static/media/Aller_It.f8ad0167.ttf"
+  },
+  {
+    "revision": "785388a9e6416c286ec82ff966c696fd",
+    "url": "/basketmondo/static/media/Aller_Rg.785388a9.ttf"
+  },
+  {
+    "revision": "22a969781fd4ff3dba17116d0c7a9440",
+    "url": "/basketmondo/index.html"
+  }
+];
