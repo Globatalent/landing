3d5a3068f66bd53499090bdbcf9fb5fb81cfc197
rubenGlobatalent <45387589+rubenGlobatalent@users.noreply.github.com>
2019-03-22 10:49:22 +0100

    up

---
+++ futmondo/precache-manifest.812615a32d69e9501646dc91f022cb43.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "58b30c2c2d6b6724b3a75482212f3ca1",
+    "url": "/futmondo/static/media/fighter.58b30c2c.jpg"
+  },
+  {
+    "revision": "bdd179a35c875ef8646a",
+    "url": "/futmondo/static/css/main.cd2398a5.chunk.css"
+  },
+  {
+    "revision": "026cb3691c16e27313bb",
+    "url": "/futmondo/static/js/1.026cb369.chunk.js"
+  },
+  {
+    "revision": "9e25ac3f61aebc429302",
+    "url": "/futmondo/static/js/runtime~main.9e25ac3f.js"
+  },
+  {
+    "revision": "6764a1f3a50559388ced290f39296f12",
+    "url": "/futmondo/static/media/hero.6764a1f3.jpg"
+  },
+  {
+    "revision": "342bba37671764654e5c92df6f792c18",
+    "url": "/futmondo/static/media/futmondo--horizontal.342bba37.png"
+  },
+  {
+    "revision": "1e58b887bddaeee5511b7e30222c0e0c",
+    "url": "/futmondo/static/media/vega.1e58b887.JPG"
+  },
+  {
+    "revision": "36c7230e976b140d7a3be96bd2c3a67e",
+    "url": "/futmondo/static/media/zentro.36c7230e.jpg"
+  },
+  {
+    "revision": "bdd179a35c875ef8646a",
+    "url": "/futmondo/static/js/main.bdd179a3.chunk.js"
+  },
+  {
+    "revision": "c27b279c3fb2538ca8bdda0d7f89bfdf",
+    "url": "/futmondo/static/media/supp_1.c27b279c.png"
+  },
+  {
+    "revision": "327654da90de0f7dd27ce686e0c77ef0",
+    "url": "/futmondo/static/media/supp_2.327654da.png"
+  },
+  {
+    "revision": "0d913f8a270b4cf90e42e955365860ff",
+    "url": "/futmondo/static/media/supp_3.0d913f8a.png"
+  },
+  {
+    "revision": "3b7476c78f5087fdc318c7ccf54088a3",
+    "url": "/futmondo/static/media/supp_4.3b7476c7.png"
+  },
+  {
+    "revision": "e939c85f4c3e5acce84abe2bf2e94206",
+    "url": "/futmondo/static/media/epfc.e939c85f.jpg"
+  },
+  {
+    "revision": "f8ad016722fe04c29eb0ad3a12fcdfc7",
+    "url": "/futmondo/static/media/Aller_It.f8ad0167.ttf"
+  },
+  {
+    "revision": "785388a9e6416c286ec82ff966c696fd",
+    "url": "/futmondo/static/media/Aller_Rg.785388a9.ttf"
+  },
+  {
+    "revision": "789bfe13a63c2157da6edb8230734556",
+    "url": "/futmondo/index.html"
+  }
+];
